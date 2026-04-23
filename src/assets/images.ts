03b0e066c74c4d76870c3dc8ddbdf40a3e4286eb
Nagy Lorinc Ambrus <ambrus.lorinc.nagy@idomsoft.hu>
2026-04-23 09:30:16 +0200

    implement vercel connection

---
+++ src/assets/images.ts
@@ -0,0 +1,35 @@
+import bg1 from "./alex-knight-Ys-DBJeX0nE-unsplash.jpg";
+import bg2 from "./bojun-liu-M3T9qfa_kDQ-unsplash.jpg";
+import bg3 from "./cheng-feng-4pTY4ty7jXM-unsplash.jpg";
+import bg4 from "./cheng-feng-psdV2Rl-GvU-unsplash.jpg";
+import bg5 from "./chi-hung-wong-pmfSR43nDTc-unsplash.jpg";
+import bg6 from "./japan.jpg";
+import bg7 from "./jony-y-hWMp_XLCB8k-unsplash.jpg";
+import bg8 from "./kevin-laminto-7PqRZK6rbaE-unsplash.jpg";
+import bg9 from "./levon-vardanyan-ihUVzI4f5To-unsplash.jpg";
+import bg10 from "./pexels-jplenio-2080960.jpg";
+import bg11 from "./ray-zhuang-kL3u4Tqfn1s-unsplash.jpg";
+import bg12 from "./salty-justice-NOMebOREvtc-unsplash.jpg";
+import bg13 from "./sergio-rola-bVM7IO7pt7s-unsplash.jpg";
+import bg14 from "./valentin-beauvais-yVUQlyRlJSw-unsplash.jpg";
+import bg15 from "./van-mendoza-r7YZXv5f5cc-unsplash.jpg";
+import bg16 from "./vitaly-otinov-BzuUDHCi_vo-unsplash.jpg";
+
+export const images = [
+  bg1,
+  bg2,
+  bg3,
+  bg4,
+  bg5,
+  bg6,
+  bg7,
+  bg8,
+  bg9,
+  bg10,
+  bg11,
+  bg12,
+  bg13,
+  bg14,
+  bg15,
+  bg16,
+];
