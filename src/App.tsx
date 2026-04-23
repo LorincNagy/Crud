9072f3d575a678829ebd9480843e859e7f1235f2
Nagy Lorinc Ambrus <ambrus.lorinc.nagy@idomsoft.hu>
2026-04-23 09:20:02 +0200

    implement vercel connection

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
-import { images } from "./assets/images.ts";
+import { images } from "./assets/images";
 import AppRouter from "./router/Approuter";
 
 function App() {
@@ -11,13 +11,12 @@ function App() {
     return images[randomIndex];
   });
 
-  // Szükségünk van az előző képre is, hogy ne fekete legyen alatta
   const [prevBg, setPrevBg] = useState<string>(bgUrl);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setBgUrl((currentBg: string) => {
-        setPrevBg(currentBg); // Mielőtt váltunk, elmentjük a régit háttérnek
+        setPrevBg(currentBg);
         let nextIndex;
         do {
           nextIndex = Math.floor(Math.random() * images.length);
@@ -29,7 +28,6 @@ function App() {
     return () => clearInterval(intervalId);
   }, []);
 
-  // Segédfüggvény a tartalom típusának eldöntéséhez
   const renderBackground = (url: string) => {
     const isVideo = url.toLowerCase().endsWith(".mp4");
 
@@ -56,15 +54,10 @@ function App() {
   return (
     <BrowserRouter>
       <div className="relative min-h-screen w-full flex flex-col bg-black overflow-hidden">
-        {/* ALSÓ RÉTEG */}
         <div>{renderBackground(prevBg)}</div>
-
-        {/* FELSŐ RÉTEG (Animált) */}
         <div key={bgUrl} className="animate-fade-in z-0">
           {renderBackground(bgUrl)}
         </div>
-
-        {/* TARTALOM */}
         <div className="relative z-10 flex flex-col min-h-screen">
           <Header />
           <main className="grow p-4 md:p-8">
